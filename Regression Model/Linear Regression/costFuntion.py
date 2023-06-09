3ac4de6e13e423f2a293b88b74a921c951c57019
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2023-06-10 00:42:35 +0530

    Update costFuntion.py

--- Regression Model/Linear Regression/costFuntion.py
+++ Regression Model/Linear Regression/costFuntion.py
@@ -16,9 +16,10 @@ def compute_cost(x,y,w,b):
     m = x.shape[0]
     cost_sum = 0
     for i in range(m):
-        cost = ((w*x[i] + b) - y[i])**2
-        cost_sum = cost_sum + cost
-    total_cost = (1/2*m)*cost_sum
+        f_wb = w * x[i] + b
+        cost = (f_wb - y[i]) ** 2
+        cost_sum = cost_sum + cost 
+    total_cost = (1/(2*m))*cost_sum 
 
     return total_cost
 
