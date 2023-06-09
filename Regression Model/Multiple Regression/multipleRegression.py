8799412019e44b2109fd5629c7d44a7c079417dd
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2023-06-09 22:04:02 +0530

    Update multipleRegression.py

--- Regression Model/Multiple Regression/multipleRegression.py
+++ Regression Model/Multiple Regression/multipleRegression.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 x_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
 y_train = np.array([460, 232, 178])
 
-b_init = 785.1811367994083
-w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+b_init = 0.0
+w_init = np.array([ 0.0, 0.0, 0.0, 0.0])
 
 def compute_cost(x,y,w,b):
 
