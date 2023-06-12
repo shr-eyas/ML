0f2963c56a6396f2067cdc521b71e6eba65f82ff
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2023-06-12 23:44:39 +0530

    Update and rename Regression/polynomialRegression.py to Regression/Polynomial Regression/polynomialRegression.py

--- Regression/Polynomial Regression/polynomialRegression.py
+++ Regression/Polynomial Regression/polynomialRegression.py
@@ -4,6 +4,7 @@ import numpy as np
 x_train = np.array([1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22])
 y_train = np.array([100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100])
 
+cost_values = []
 
 def compute_cost(x,y,w1,w2,b):
 
@@ -48,13 +49,14 @@ def gradient_descent(x,y,w1_in,w2_in,b_in,alpha,num_iters,cost_function,gradient
         b = b - alpha*dj_db
         w1 = w1 - alpha*dj_dw1
         w2 = w2 - alpha*dj_dw2
+        cost_values.append(compute_cost(x, y, w1, w2, b))
     return w1, w2, b
 
 w1_init = 0
 w2_init = 0
 b_init = 0
 
-iterations = 10000
+iterations = 1000000
 tmp_alpha = 0.00001
 
 w1_final, w2_final, b_final = gradient_descent(x_train ,y_train, w1_init, w2_init, b_init, tmp_alpha, iterations, compute_cost, compute_gradient)
@@ -71,10 +73,16 @@ def predict(x, w1, w2, b):
 x_range = np.linspace(min(x_train), max(x_train), 100)
 y_predicted = predict(x_range, w1_final, w2_final, b_final)
 
+# plotting the cost values
+plt.plot(range(iterations), cost_values)
+plt.xlabel('Iteration')
+plt.ylabel('Cost')
+plt.title('Cost vs. Iteration')
+plt.show()
+
 plt.scatter(x_train, y_train)
 plt.plot(x_range, y_predicted, color='red')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Polynomial Regression')
-
 plt.show()
